8c7fffa47bd3aa40bb44f0b9d8d27dae43c85a02
Cesar Lopez <cesarl@outlook.com>
2016-11-28 09:50:15 -0600

    Modal Events

--- js/site.js
+++ js/site.js
@@ -9,4 +9,18 @@
     $pickButton.text(reason);
   });
   
+  var $sentDialog = $("#sentDialog");
+  
+  $("#contactForm").on("submit", function () {
+    
+    $sentDialog.modal('show');
+    
+    return false; // Do not complete form submition
+  });
+  
+  
+  $sentDialog.on("hidden.bs.modal", function () {
+    alert("close");
+  });
+  
 })();
